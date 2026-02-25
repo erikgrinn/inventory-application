ed27f2042bfcc5b6887e7bed9fc6e778f4911c6e
Erik Grinn <grinn.erik@gmail.com>
2026-02-25 11:22:21 -0500

    DDL

--- server/db/shebang.js
+++ server/db/shebang.js
@@ -9,30 +9,18 @@ const { Client } = require("pg");
 const DB_USER = process.env.DB_USER;
 const DB_PASSWORD = process.env.DB_PASSWORD;
 
-const SQLDrop = `
-DROP TABLE usernames;
-`;
+
 
 const SQL = `
-CREATE TABLE IF NOT EXISTS usernames (
-  id INTEGER PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
-  username VARCHAR ( 255 )
-);
 
-INSERT INTO usernames (username) 
-VALUES
-  ('Bryan'),
-  ('Odin'),
-  ('Zeus');
 `;
 
 async function seed() {
   console.log("seeding...");
   const client = new Client({
-    connectionString: `postgresql://${DB_USER}:${DB_PASSWORD}@localhost:5432/top_users`,
+    connectionString: `postgresql://${DB_USER}:${DB_PASSWORD}@localhost:5432/inventory_application`,
   });
   await client.connect();
-  await client.query(SQLDrop)
   await client.query(SQL);
   await client.end();
   console.log("done");
